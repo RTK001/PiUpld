eeb95a039ccf184ff1c68f6f6f94e87f8c15d050
Rishi <rishikalra001@gmail.com>
2019-07-27 23:25:51 +0100

    updated key

--- PiUpld/settings.py
+++ PiUpld/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/2.1/ref/settings/
 """
 
 import os
+from .key import SECRET_KEY
 from socket import gethostname, gethostbyname
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
@@ -20,8 +21,6 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/2.1/howto/deployment/checklist/
 
-# SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'i0)nao#!3r^ml0mm!ouf(f_v!%$kmxya1#9*35gx=%@_s^6%t-'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
