bb4c1f25e1e50aaa4e28901ad7ebdff366c368c0
Rishi <rishikalra001@gmail.com>
2019-07-28 15:12:37 +0100

    Added view and html templates

--- Upld/views.py
+++ Upld/views.py
@@ -1,3 +1,9 @@
 from django.shortcuts import render
+from django.views.generic.base import TemplateView
+from .models import project
 
-# Create your views here.
+class IndexView(TemplateView):
+    template_name = "Upld/index.html"
+
+    def get_context_data(self, **kwargs):
+        return super().get_context_data(**kwargs)
